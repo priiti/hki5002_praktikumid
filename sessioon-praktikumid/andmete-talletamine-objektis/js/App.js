2f760de1e752ae69c063c61a121913df2570500a
Priit Pärl <priitparl@gmail.com>
2017-04-26 23:13:16 +0300

    added canvas

--- sessioon-praktikumid/andmete-talletamine-objektis/js/App.js
+++ sessioon-praktikumid/andmete-talletamine-objektis/js/App.js
@@ -1,4 +1,5 @@
 window.onload = () => {
 	const calculator1 = new Calculator('first');
 	const calculator2 = new Calculator('second');
+	// const board = new Canvas(400, 400, 'root');
 };
--- sessioon-praktikumid/andmete-talletamine-objektis/js/Calculator.js
+++ sessioon-praktikumid/andmete-talletamine-objektis/js/Calculator.js
@@ -1,11 +1,10 @@
 class Calculator {
 	constructor(destination) {
-		document.body.appendChild(DomElements.createDivElement('warehouse'));
-		document.querySelector(`#warehouse`).appendChild(DomElements.createParagraphElement('info', 'Andmete talletamine objektis'));
-		document.querySelector(`#warehouse`).appendChild(DomElements.createButtonElement(`left_btn_${destination}`, 'Low'));
-		document.querySelector(`#warehouse`).appendChild(DomElements.createInputElement(`${destination}_input`, 0));
-		document.querySelector(`#warehouse`).appendChild(DomElements.createSelectElement(`${destination}_select`));
-		document.querySelector(`#warehouse`).appendChild(DomElements.createButtonElement(`right_btn_${destination}`, 'High'));
+		document.querySelector(`#root`).appendChild(DomElements.createParagraphElement('info', 'Andmete talletamine objektis'));
+		document.querySelector(`#root`).appendChild(DomElements.createButtonElement(`left_btn_${destination}`, 'Low'));
+		document.querySelector(`#root`).appendChild(DomElements.createInputElement(`${destination}_input`, 0));
+		document.querySelector(`#root`).appendChild(DomElements.createSelectElement(`${destination}_select`));
+		document.querySelector(`#root`).appendChild(DomElements.createButtonElement(`right_btn_${destination}`, 'High'));
 		document.querySelector(`#left_btn_${destination}`).addEventListener('click', this.lowerInventoryValue.bind(this));
 		document.querySelector(`#right_btn_${destination}`).addEventListener('click', this.raiseInventoryValue.bind(this));
 
