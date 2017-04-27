9264d8da991920fc16770be336bf3b82975989b2
Priit PÃrl <priitparl@gmail.com>
2017-04-27 14:37:46 +0300

    Ãchanges

--- sessioon-praktikumid/textbox-input-calculator/App.js
+++ sessioon-praktikumid/textbox-input-calculator/App.js
@@ -10,20 +10,20 @@ class App {
 		this.applicationSetup();
 	}
 	applicationSetup() {
-		document.querySelector('body').
-			appendChild(Elements.createHeading('h1', 'Aritmeetiline keskmine'));
-		document.querySelector('body').
-			appendChild(Elements.createDivElement('textbox_section'));
-		document.querySelector('body').
-			appendChild(Elements.createButton('add_textbox_btn', this, 'Lisa uus kast'));
-		document.querySelector('body').
-			appendChild(Elements.createButton('calculate_btn', this, 'Arvuta keskmine'));	
+		document.querySelector('body')
+			.appendChild(Elements.createHeading('h1', 'Aritmeetiline keskmine'));
+		document.querySelector('body')
+			.appendChild(Elements.createDivElement('textbox_section'));
+		document.querySelector('body')
+			.appendChild(Elements.createButton('add_textbox_btn', this, 'Lisa uus kast'));
+		document.querySelector('body')
+			.appendChild(Elements.createButton('calculate_btn', this, 'Arvuta keskmine'));	
 		document.querySelector('#add_textbox_btn')
 			.addEventListener('click', this.createBox.bind(this));
 		document.querySelector('#calculate_btn')
 			.addEventListener('click', this.calculate.bind(this));
-		document.querySelector('body').
-			appendChild(Elements.createDivElement('answer'));
+		document.querySelector('body')
+			.appendChild(Elements.createDivElement('answer'));
 	}
 	createBox() {
 		this.addNewBoxes(new Textbox(this._count));
