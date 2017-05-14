43a4274fd648f9d266a4f698d1a98b832a4a0dc2
Priit Pärl <priitparl@gmail.com>
2017-05-14 23:21:59 +0300

    Circle

--- sessioon-praktikumid/ristmik/src/modules/Circle.js
+++ sessioon-praktikumid/ristmik/src/modules/Circle.js
@@ -1,9 +1,14 @@
 class Circle {
-	constructor(x, y, radius) {
+	constructor(x, y, radius, color) {
 		this.x = x;
 		this.y = y;
 		this.radius = radius;
+		this.color = color;
 		this.context = undefined;
+	}
+
+	set color(color) {
+		this.color = color;
 	}
 
 	draw(context) {
@@ -11,7 +16,9 @@ class Circle {
 		if (this.context === null) {
 			return;
 		}
-
-
+		this.context.beginPath();
+		this.context.fillStyle = this.color;
+		this.context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+		this.context.fill();
 	}
 }
