fd7d40b4e2be7c71ff931410e172cdde3620a747
Priit Pärl <priitparl@gmail.com>
2017-05-22 15:15:43 +0300

    change

--- fetch/index.js
+++ fetch/index.js
@@ -1,11 +1,10 @@
-function getData(url) {
-    fetch(url)
-        .then((data) => {
-            console.log(data);
-        })
-        .catch((err) => {
-            throw Error(err);
-        });
+const url = 'http://tigu.hk.tlu.ee/~priit.parl/programmeerimine-1-js/fetch/time.php';
+
+async function getData(url) {
+	const data = await fetch(url);
+	const time = await data.json();
+	
+	document.querySelector('#time-section').innerHTML = time;
 }
 
 setInterval(() => {
