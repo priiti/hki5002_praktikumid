f55c8ee46daf98f56df1663bdf46de892206726d
Priit Pärl <priitparl@gmail.com>
2017-05-23 11:29:56 +0300

    eventemitter

---
+++ eventemitter/index.js
@@ -0,0 +1,31 @@
+const EventEmitter = require('events').EventEmitter;
+
+const getResource = (value) => {
+    const e = new EventEmitter();
+    process.nextTick(() => {
+        let count = 0;
+        e.emit('start');
+        let t = setInterval(() => {
+            e.emit('data', count++);
+            if (count === value) {
+                e.emit('end', count);
+                clearInterval(t);
+            }
+        }, 1000);
+    });
+    return e;
+};
+
+const r = getResource(5);
+
+r.on('start', () => {
+    console.log('Started...');
+});
+
+r.on('data', d => {
+    console.log(`Received data is ${d}`);
+});
+
+r.on('end', t => {
+    console.log(`Events done: ${t}`);
+});
